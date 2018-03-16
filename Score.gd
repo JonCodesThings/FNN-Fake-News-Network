5f648905841809dfba0a7921a7762153e927c0de
giodestone <37457035+giodestone@users.noreply.github.com>
2018-03-16 22:53:30 +0000

    Started working on buttons to test the scoring system.

--- Score.gd
+++ Score.gd
@@ -46,3 +46,7 @@ func SaveToFile(path):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_Add_To_Score_pressed():
+	pass # replace with function body
