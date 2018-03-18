a60a8e31f57b854f013f266cb84e3bbdb648f82d
JonDuncanson-abertay <1600538@uad.ac.uk>
2018-03-18 01:35:25 +0000

    First bit of headline output done

--- Scripts/Headline.gd
+++ Scripts/Headline.gd
@@ -1,13 +1,17 @@
-extends Node
+extends Label
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+#var m_font = Font.new()
+#m_font = preload("res://Fonts/runescape_uf/runescape_uf.tres")
 var m_text = ""
 var m_score = 0
 var m_selected = false
 
 func _ready():
+	self.set("custom_fonts/fonts", load("res://Fonts/runescape_uf/runescape_uf.tres"))
+	self.set_size(Vector2(300, 300))
 	# Called every time the node is added to the scene.
 	# Initialization here
 	m_text = ""
@@ -29,10 +33,12 @@ func set_score(score):
 
 func set_text(text):
 	m_text = text
+	self.text = str(m_text)
 	
 func set_selected(selected):
 	m_selected = selected
-#func _process(delta):
+	
+func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	pass
+	pass
--- Scripts/HeadlineManager.gd
+++ Scripts/HeadlineManager.gd
@@ -6,11 +6,12 @@ extends Node
 var headline = preload("res://Scripts/Headline.gd")
 var headline_left = headline.new()
 var headline_right = headline.new()
-var string_arr = []
-var score_arr = []
+var string_arr = PoolStringArray()
+var score_arr = PoolIntArray()
 var left_child_index = -1
 var right_child_index = -1
 var headline_count = 5
+var deltacount = 0
 
 func load_headlines(path):
 	var file = File.new()
@@ -18,14 +19,15 @@ func load_headlines(path):
 	if file.file_exists(path):
 		file.open(path, file.READ)
 		while !file.eof_reached():
-			string_arr.push_back(file.get_csv_line(","))
-			score_arr.push_back(file.get_csv_line(","))
+			string_arr.push_back(str(file.get_line()))
+			score_arr.push_back(int(file.get_line()))
+			print(string_arr[string_arr.size() - 1])
 		file.close()
 	pass
 	
 func generate_headlines():
 	var random = randi()%headline_count+1
-	get_child(left_child_index).set_text(string_arr[random])
+	get_child(left_child_index).set_text(str(string_arr[random]))
 	get_child(left_child_index).set_score(score_arr[random])
 	random = randi()%headline_count+1
 	get_child(right_child_index).set_text(string_arr[random])
@@ -33,9 +35,9 @@ func generate_headlines():
 	
 	while get_child(left_child_index).get_score() == get_child(right_child_index).get_score():
 		random = randi()%headline_count+1
-		get_child(right_child_index).set_text(string_arr[random])
+		get_child(right_child_index).set_text(str(string_arr[random]))
 		get_child(right_child_index).set_score(score_arr[random])
-		
+
 	pass
 
 func _ready():
@@ -45,12 +47,16 @@ func _ready():
 	add_child(headline_right)
 	left_child_index = headline_left.get_index()
 	right_child_index = headline_right.get_index()
-	load_headlines("res://data.csv")
+	headline_left.set_position(Vector2(0, 0))
+	headline_right.set_position(Vector2(800, 200))
+	load_headlines("res://data.data")
 	generate_headlines()
 	pass
 
 func _process(delta):
-	generate_headlines()
+	#eltacount += delta
+	#f delta > 1000:
+		#generate_headlines()
 	pass
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
